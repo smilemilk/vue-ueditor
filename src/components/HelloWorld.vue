ebcde810f439288c01214bc1ea3c6d640100dfcc
gexuhui <xuhui.ge@yoho.cn>
2017-10-10 18:08:35 +0800

    vue-cli

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="opt-area">
+    <div class="user-pic">
+      <img src=""/>
+    </div>
+    <div class="opt-area-detail">
+      <form id="form" method="post" target="_blank">
+        <script type="text/plain" id="myEditor" name="myEditor">
+          <p>999999</p>
+        </script>
+      </form>
+    </div>
+    <div class="opt-area-btn">
+      <button>回答</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
